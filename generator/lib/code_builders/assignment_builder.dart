46cc0d6851eed3010c46c2571555213ac80a90bc
flowing_sky <flowing_sky@126.com>
2022-08-23 00:04:45 +0800

    refactor: nested mapping builder
    Extract the code about the nested mapping from the
    'class_builder.dart' to the new file
    'nested_mapping.builder.dart'.

--- generator/lib/code_builders/assignment_builder.dart
+++ generator/lib/code_builders/assignment_builder.dart
@@ -3,6 +3,7 @@ import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:code_builder/code_builder.dart';
 import 'package:smartstruct_generator/models/source_assignment.dart';
+import './nested_mapping_builder.dart';
 
 /// Generates an assignment of a reference to a sourcefield.
 ///
@@ -48,7 +49,7 @@ Expression generateSourceFieldAssignment(SourceAssignment sourceAssignment,
     if (sourceAssignment.shouldAssignList(targetField.type)) {
       final sourceListType = _getGenericTypes(sourceField.type).first;
       final targetListType = _getGenericTypes(targetField.type).first;
-      final matchingMappingListMethods = _findMatchingMappingMethod(
+      final matchingMappingListMethods = findMatchingMappingMethod(
           abstractMapper, targetListType, sourceListType);
 
       // mapping expression, default is just the identity,
@@ -99,7 +100,7 @@ Expression generateSourceFieldAssignment(SourceAssignment sourceAssignment,
       }
     } else {
       // found a mapping method in the class which will map the source to target
-      final matchingMappingMethods = _findMatchingMappingMethod(
+      final matchingMappingMethods = findMatchingMappingMethod(
           abstractMapper, targetField.type, sourceField.type);
 
       // nested classes can be mapped with their own mapping methods
@@ -114,88 +115,6 @@ Expression generateSourceFieldAssignment(SourceAssignment sourceAssignment,
     }
   }
   return sourceFieldAssignment;
-}
-
-Expression invokeNestedMappingFunction(
-  MethodElement method, 
-  bool sourceNullable,
-  Expression refWithQuestion,
-  Expression ref,
-) {
-  Expression sourceFieldAssignment;
-  if(method.parameters.first.isOptional) {
-    // The parameter can be null.
-    sourceFieldAssignment = refer(method.name)
-        .call([refWithQuestion]);
-  } else {
-    sourceFieldAssignment = refer(method.name)
-        .call([ref]);
-    sourceFieldAssignment = checkNullExpression(
-      sourceNullable,
-      refWithQuestion, 
-      sourceFieldAssignment
-    );
-  }
-  return sourceFieldAssignment;
-}
-
-Expression invokeNestedMappingForStaticFunction(
-  ExecutableElement sourceFunction,
-  ClassElement abstractMapper,
-  VariableElement targetField,
-  Expression sourceFieldAssignment,
-) {
-  final returnType = sourceFunction.returnType;
-  final matchingMappingMethods = _findMatchingMappingMethod(
-      abstractMapper, targetField.type, returnType);
-  if(matchingMappingMethods.isNotEmpty) {
-    final nestedMappingMethod = matchingMappingMethods.first;
-
-    if(
-      nestedMappingMethod.parameters.first.type.nullabilitySuffix != NullabilitySuffix.question &&
-      sourceFunction.returnType.nullabilitySuffix == NullabilitySuffix.question
-    ) {
-      final str = makeNullCheckCall(
-        sourceFieldAssignment.accept(
-          DartEmitter()
-        ).toString(),
-        nestedMappingMethod,
-      );
-      sourceFieldAssignment = refer(str);
-    } else {
-      sourceFieldAssignment = refer(matchingMappingMethods.first.name)
-          .call([sourceFieldAssignment]);
-    }
-
-  }
-  return sourceFieldAssignment;
-}
-
-/// Finds a matching Mapping Method in [classElement]
-/// which has the same return type as the given [targetReturnType] and same parametertype as the given [sourceParameterType]
-Iterable<MethodElement> _findMatchingMappingMethod(ClassElement classElement,
-    DartType targetReturnType, DartType sourceParameterType) {
-  final matchingMappingMethods = classElement.methods.where((met) {
-    // Sometimes the user is troubled by the nullability of these types.
-    // So ingore the nullability of all the type for the nested mapping function is more easy to be matched.
-    // The process of nullability is one duty for this library.
-
-    if(met.parameters.isEmpty) {
-        return false;
-    }
-    final metReturnElement = met.returnType.element;
-    final metParameterElement = met.parameters.first.type.element;
-
-    final targetReturnElement = targetReturnType.element;
-    final srcParameterElement = sourceParameterType.element;
-
-    return metReturnElement == targetReturnElement &&
-        (metParameterElement == srcParameterElement);
-
-    // return met.returnType == targetReturnType &&
-    //     met.parameters.isNotEmpty && met.parameters.first.type == sourceParameterType;
-  });
-  return matchingMappingMethods;
 }
 
 Iterable<DartType> _getGenericTypes(DartType type) {
@@ -217,35 +136,6 @@ Map<String, Expression> makeNamedArgumentForStaticFunction(ExecutableElement  el
         value.name, 
         refer(argumentMap[value.name]!)
       ));
-}
-
-Expression checkNullExpression(
-  bool needCheck,
-  Expression sourceRef,
-  Expression expression,
-) {
-  if(needCheck) {
-    return sourceRef.equalTo(literalNull).conditional(
-      literalNull, 
-      expression,
-    );
-  } else {
-    return expression;
-  }
-}
-
-makeNullCheckCall(
-  String checkTarget,
-  MethodElement method,
-) {
-
-  final methodInvoke = refer(method.name).call([refer("tmp")]).accept(DartEmitter()).toString();
-  return '''
-  (){
-    final tmp = $checkTarget;
-    return tmp == null ? null : $methodInvoke;
-  }()
-''';
 }
 
 checkNestMappingReturnNullable(MethodElement method, bool inputNullable) {
