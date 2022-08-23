8bc548e557b8328f01b93adf68cb7179f608f5a6
flowing_sky <flowing_sky@126.com>
2022-08-23 11:49:03 +0800

    refactor: list assignment
    Move the code about 'list assignment' in the file
    'assignment_builder.dart' to the file 'list_assignment_builder.dart'

--- generator/lib/code_builders/assignment_builder.dart
+++ generator/lib/code_builders/assignment_builder.dart
@@ -1,8 +1,8 @@
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/nullability_suffix.dart';
-import 'package:analyzer/dart/element/type.dart';
 import 'package:code_builder/code_builder.dart';
+import 'package:smartstruct_generator/code_builders/list_assignment_builder.dart';
 import 'package:smartstruct_generator/models/source_assignment.dart';
+
 import './nested_mapping_builder.dart';
 
 /// Generates an assignment of a reference to a sourcefield.
@@ -47,47 +47,7 @@ Expression generateSourceFieldAssignment(SourceAssignment sourceAssignment,
     sourceFieldAssignment = sourceReference.property(sourceField.name);
     // list support
     if (sourceAssignment.shouldAssignList(targetField.type)) {
-      final sourceListType = _getGenericTypes(sourceField.type).first;
-      final targetListType = _getGenericTypes(targetField.type).first;
-      final nestedMapping = findMatchingMappingMethod(
-          abstractMapper, targetListType, sourceListType);
-
-      // mapping expression, default is just the identity,
-      // for example for primitive types or objects that do not have their own mapping method
-      var expr = refer('(e) => e');
-      var sourceIsNullable = sourceListType.nullabilitySuffix == NullabilitySuffix.question;
-      var targetIsNullable = targetListType.nullabilitySuffix == NullabilitySuffix.question; 
-      var needTargetFilter = sourceIsNullable && !targetIsNullable;
-      if (nestedMapping != null) {
-        expr = generateNestedMappingLambda(sourceListType, nestedMapping);
-        final returnIsNullable = checkNestMappingReturnNullable(nestedMapping, sourceIsNullable);
-        needTargetFilter = !targetIsNullable && returnIsNullable; 
-      }
-
-      sourceFieldAssignment =
-          // source.{field}.map
-        sourceReference.property(sourceField.name)
-        .property('map')
-        // (expr)
-        .call([expr]);
-
-      if(needTargetFilter) {
-        sourceFieldAssignment = sourceFieldAssignment.property("where").call([refer("(x) => x != null")]);
-      }
-
-      if(sourceAssignment.needCollect(targetField.type)) {
-        sourceFieldAssignment = sourceFieldAssignment
-          //.toList() .toSet()
-          .property(sourceAssignment.collectInvoke(targetField.type))
-          // .property('toList')
-          // .call([])
-          ;
-      }
-
-      if(needTargetFilter) {
-        sourceFieldAssignment = sourceFieldAssignment
-          .asA(refer(targetField.type.getDisplayString(withNullability: true)));
-      }
+      sourceFieldAssignment = generateListAssignment(sourceAssignment, abstractMapper, targetField);
     } else {
         sourceFieldAssignment = generateNestedMapping(
           abstractMapper,
@@ -100,9 +60,6 @@ Expression generateSourceFieldAssignment(SourceAssignment sourceAssignment,
   return sourceFieldAssignment;
 }
 
-Iterable<DartType> _getGenericTypes(DartType type) {
-  return type is ParameterizedType ? type.typeArguments : const [];
-}
 
 // Sometimes we need to pass some variable to the static function just like "this pointer".
 // We can use the named parameters to implement the goal.
@@ -119,16 +76,4 @@ Map<String, Expression> makeNamedArgumentForStaticFunction(ExecutableElement  el
         value.name, 
         refer(argumentMap[value.name]!)
       ));
-}
-
-checkNestMappingReturnNullable(MethodElement method, bool inputNullable) {
-  final returnIsNullable = 
-    (inputNullable && 
-      method.parameters.first.type.nullabilitySuffix != NullabilitySuffix.question
-    ) ||
-    (
-      inputNullable &&
-      method.returnType.nullabilitySuffix == NullabilitySuffix.question
-    );
-    return returnIsNullable;
 }
---
+++ generator/lib/code_builders/list_assignment_builder.dart
@@ -0,0 +1,75 @@
+
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/nullability_suffix.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:code_builder/code_builder.dart';
+import 'package:smartstruct_generator/models/source_assignment.dart';
+
+import 'nested_mapping_builder.dart';
+
+generateListAssignment(SourceAssignment sourceAssignment,
+    ClassElement abstractMapper, VariableElement targetField
+) {
+  final sourceField = sourceAssignment.field!;
+  final sourceReference = refer(sourceAssignment.sourceName!);
+
+  final sourceListType = _getGenericTypes(sourceField.type).first;
+  final targetListType = _getGenericTypes(targetField.type).first;
+  final nestedMapping = findMatchingMappingMethod(
+      abstractMapper, targetListType, sourceListType);
+
+  // mapping expression, default is just the identity,
+  // for example for primitive types or objects that do not have their own mapping method
+  var expr = refer('(e) => e');
+  var sourceIsNullable = sourceListType.nullabilitySuffix == NullabilitySuffix.question;
+  var targetIsNullable = targetListType.nullabilitySuffix == NullabilitySuffix.question; 
+  var needTargetFilter = sourceIsNullable && !targetIsNullable;
+  if (nestedMapping != null) {
+    expr = generateNestedMappingLambda(sourceListType, nestedMapping);
+    final returnIsNullable = checkNestMappingReturnNullable(nestedMapping, sourceIsNullable);
+    needTargetFilter = !targetIsNullable && returnIsNullable; 
+  }
+
+  Expression sourceFieldAssignment =
+      // source.{field}.map
+    sourceReference.property(sourceField.name)
+    .property('map')
+    // (expr)
+    .call([expr]);
+
+  if(needTargetFilter) {
+    sourceFieldAssignment = sourceFieldAssignment.property("where").call([refer("(x) => x != null")]);
+  }
+
+  if(sourceAssignment.needCollect(targetField.type)) {
+    sourceFieldAssignment = sourceFieldAssignment
+      //.toList() .toSet()
+      .property(sourceAssignment.collectInvoke(targetField.type))
+      // .property('toList')
+      // .call([])
+      ;
+  }
+
+  if(needTargetFilter) {
+    sourceFieldAssignment = sourceFieldAssignment
+      .asA(refer(targetField.type.getDisplayString(withNullability: true)));
+  }
+
+  return sourceFieldAssignment;
+}
+
+Iterable<DartType> _getGenericTypes(DartType type) {
+  return type is ParameterizedType ? type.typeArguments : const [];
+}
+
+checkNestMappingReturnNullable(MethodElement method, bool inputNullable) {
+  final returnIsNullable = 
+    (inputNullable && 
+      method.parameters.first.type.nullabilitySuffix != NullabilitySuffix.question
+    ) ||
+    (
+      inputNullable &&
+      method.returnType.nullabilitySuffix == NullabilitySuffix.question
+    );
+    return returnIsNullable;
+}
